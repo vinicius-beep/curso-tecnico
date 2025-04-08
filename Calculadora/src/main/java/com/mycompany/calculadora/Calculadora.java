5c01ff845ddfb91334f0675763ea74e16242d762
Vini <viniciusdasilvadossantos4@gmail.com>
2025-04-07 21:09:37 -0300

    interface

---
+++ Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author 08199
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
